a776f04296045ce66b9eaf90e09de66a40010c5d
5er9en <61845734+SergeyRyskin@users.noreply.github.com>
2022-05-06 03:30:18 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/word_hunter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.word_hunter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
